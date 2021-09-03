748e4ffe3b4a97530827014b630b6d93a1977544
MetroByte <metrobyte@protonmail.com>
2021-09-03 06:50:16 +0500

    Update SampleApp.py

--- examples/SampleApp.py
+++ examples/SampleApp.py
@@ -1,24 +1,26 @@
-﻿from PyQt4 import QtGui
 import sys
 
-from PropertyEditor.PropertyEditorWidget import PropertyEditorWidget
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QPushButton, QApplication, QMainWindow
+
+from PropertyEditorWidget import PropertyEditorWidget
 from TestClass import TestClass
 
 
-class SampleApp(QtGui.QMainWindow):
-    def __init__(self, parent = None):
+class SampleApp(QMainWindow):
+    def __init__(self, parent=None):
         super(SampleApp, self).__init__(parent)
         self.setWindowTitle("Property Editor")
 
-        self.widget = QtGui.QWidget()
-        self.layout = QtGui.QVBoxLayout()
+        self.widget = QWidget()
+        self.layout = QVBoxLayout()
         self.widget.setLayout(self.layout)
 
         self.nodeEdit = PropertyEditorWidget()
 
-        self.button = QtGui.QPushButton("Print")
+        self.button = QPushButton("Print")
 
         self.test_class = TestClass(self)
+
         self.button.clicked.connect(self.get_dict)
 
         self.nodeEdit.addObject(self.test_class)
@@ -30,11 +32,11 @@ class SampleApp(QtGui.QMainWindow):
         self.show()
 
     def get_dict(self):
-        print self.test_class.__dict__
+        print(self.test_class.__dict__)
 
 
 if __name__ == "__main__":
-    app = QtGui.QApplication(sys.argv)
+    app = QApplication(sys.argv)
     window = SampleApp()
     window.show()
     sys.exit(app.exec_())
