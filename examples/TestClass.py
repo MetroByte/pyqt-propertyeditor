1fd05e5fd4d096206c6b10b58adf22186b936f7c
MetroByte <metrobyte@protonmail.com>
2021-09-03 06:45:49 +0500

    Update TestClass.py

--- examples/TestClass.py
+++ examples/TestClass.py
@@ -1,15 +1,12 @@
-﻿from PyQt4 import QtCore, QtGui
+from PySide6.QtCore import QObject
 
 
-class TestClass(QtCore.QObject):
+class TestClass(QObject):
 
-    def __init__(self, parent = None):
+    def __init__(self, parent=None):
         super(TestClass, self).__init__(parent)
         self.name = 'Czesc'
         self.number = 4
-        #self.color = QtGui.QColor(4)
         self.radius = 0.00
         self._id = 5
         self.test = True
-        #self.lst = [1,2,3,4,5]
-        #self.dict = {'abc' : 1, 'def' : 2}
